5e7f2dee35badfd0de81f79a9e97463c719a766f
pfischi <pfischi@gmx.de>
2014-01-11 14:20:53 +0100

    changed socket server handler to StreamRequestHandler now we can use rfile.readline()

--- sonos_server.py
+++ sonos_server.py
@@ -2,13 +2,13 @@
 
 import socketserver
 import argparse
-
 from sonos_commands import Command
 from sonos_database import SonosDatabase
 from utils import really_unicode
 
 
-class SonosTCPHandler(socketserver.BaseRequestHandler):
+class SonosTCPHandler(socketserver.StreamRequestHandler):
+
     def handle(self):
 
         # self.request is the TCP socket connected to the client
@@ -19,13 +19,22 @@ class SonosTCPHandler(socketserver.BaseRequestHandler):
         override the handle() method to implement communication to the
         client.
         """
+
         self.database = SonosDatabase(database)
-        self.data = self.request.recv(1024).strip().lower()
 
-        command = really_unicode(self.data)
+        while True:
+		
+            self.data = self.rfile.readline().strip().lower()
+            #self.data = self.request.recv(1024).strip().lower()
 
-        s_command = Command(command, self.database)
-        self.request.sendall(s_command.do_work())
+            if not self.data:
+                break
+
+            print(self.data)
+
+            command = really_unicode(self.data)
+            s_command = Command(command, self.database)
+            self.request.sendall(s_command.do_work())
 
 
 parser = argparse.ArgumentParser()
@@ -35,18 +44,13 @@ args = parser.parse_args()
 database = args.database
 
 
-
-
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
 
-
     # Create the server, binding to localhost on port 9999
-
-
     server = socketserver.TCPServer((HOST, PORT), SonosTCPHandler)
-
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
