8c3aabed5972aef5eef2510d626018dfaf14c330
pfischi <pfischi@gmx.de>
2014-01-23 00:41:24 +0100

    redesign

--- server.sonos/udp_broker.py
+++ server.sonos/udp_broker.py
@@ -1,4 +1,5 @@
 import socket
+import sonos_service
 
 __author__ = 'pfischi'
 
@@ -6,30 +7,89 @@ import sys
 #sys.path.append('/usr/smarthome/plugins/sonos/server/pycharm-debug-py3k.egg')
 #import pydevd
 
+registered_clients = {}
+
+
+class UdpResponse():
+    @staticmethod
+    def get_speaker(uid):
+        if uid in sonos_service.sonos_speakers:
+            return sonos_service.sonos_speakers[uid]
+        return None
+
+    @staticmethod
+    def mute(uid):
+        speaker = UdpResponse.get_speaker(uid)
+        if speaker:
+            return "speaker/{}/mute/{}".format(uid, speaker.mute)
+
+    @staticmethod
+    def led(uid):
+        speaker = UdpResponse.get_speaker(uid)
+        if speaker:
+            return "speaker/{}/led/{}".format(uid, speaker.led)
+
+    @staticmethod
+    def volume(uid):
+        speaker = UdpResponse.get_speaker(uid)
+        if speaker:
+            return "speaker/{}/volume/{}".format(uid, speaker.volume)
+
+    @staticmethod
+    def streamtype(uid):
+        speaker = UdpResponse.get_speaker(uid)
+        if speaker:
+            return "speaker/{}/streamtype/{}".format(uid, speaker.streamtype)
+
+    @staticmethod
+    def stop(uid):
+        speaker = UdpResponse.get_speaker(uid)
+        if speaker:
+            return "speaker/{}/stop/{}".format(uid, speaker.stop)
+
+    @staticmethod
+    def play(uid):
+        speaker = UdpResponse.get_speaker(uid)
+        if speaker:
+            return "speaker/{}/play/{}".format(uid, speaker.play)
+
+    @staticmethod
+    def pause(uid):
+        speaker = UdpResponse.get_speaker(uid)
+        if speaker:
+            return "speaker/{}/pause/{}".format(uid, speaker.pause)
+
+    @staticmethod
+    def track(uid):
+        speaker = UdpResponse.get_speaker(uid)
+        if speaker:
+            return "speaker/{}/track/{}".format(uid, speaker.track)
+
+
 class UdpBroker():
 
-    def __init__(self):
-        self.registered_clients = {}
-
-    def subscribe_client(self, ip, port):
-        if ip not in self.registered_clients:
-            self.registered_clients.update({ip: [port]})
+    @staticmethod
+    def subscribe_client(ip, port):
+        if ip not in registered_clients:
+            registered_clients.update({ip: [port]})
         else:
-            ports = self.registered_clients[ip]
+            ports = registered_clients[ip]
             if port not in ports:
-                self.registered_clients[ip].append(port)
+                registered_clients[ip].append(port)
 
-    def unsubscribe_client(self, ip, port):
-        if ip in self.registered_clients:
-            if port in self.registered_clients[ip]:
-                self.registered_clients[ip].remove(port)
+    @staticmethod
+    def unsubscribe_client(ip, port):
+        if ip in registered_clients:
+            if port in registered_clients[ip]:
+                registered_clients[ip].remove(port)
 
-    def udp_send(self, data):
+    @staticmethod
+    def udp_send(data):
         try:
 
             print("sending data to all connected clients: {}".format(data))
 
-            for host, ports in self.registered_clients.items():
+            for host, ports in registered_clients.items():
                 for port in ports:
                     try:
                         family, type, proto, canonname, sockaddr = socket.getaddrinfo(host, port)[0]
@@ -37,7 +97,7 @@ class UdpBroker():
                         sock.sendto(data.encode('utf-8'), (sockaddr[0], sockaddr[1]))
                         sock.close()
                         print("UDP: Sending data to {}:{}: ".format(host, port, data))
-                        del(sock)
+                        del (sock)
 
                     except Exception as e:
                         raise Exception("UDP: Problem sending data to {}:{}: ".format(host, port, e))
