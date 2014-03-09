e1753bcfa7d872ed5f8ce426282a846a0349790b
pfischi <pfischi@gmx.de>
2014-03-09 11:23:48 +0100

    new commands code improvements better structure better radio station handling

--- server.sonos/lib_sonos/udp_broker.py
+++ server.sonos/lib_sonos/udp_broker.py
@@ -2,84 +2,10 @@
 __author__ = 'pfischi'
 
 import socket
-from lib_sonos import sonos_speaker
-
+#from lib_sonos import sonos_speaker
+import json
 
 registered_clients = {}
-
-class UdpResponse():
-    @staticmethod
-    def get_speaker(uid):
-        if uid in sonos_speaker.sonos_speakers:
-            return sonos_speaker.sonos_speakers[uid]
-        return None
-
-    @staticmethod
-    def mute(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/mute/{}".format(uid, speaker.mute)
-
-    @staticmethod
-    def led(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/led/{}".format(uid, speaker.led)
-
-    @staticmethod
-    def volume(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/volume/{}".format(uid, speaker.volume)
-
-    @staticmethod
-    def streamtype(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/streamtype/{}".format(uid, speaker.streamtype)
-
-    @staticmethod
-    def stop(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/stop/{}".format(uid, speaker.stop)
-
-    @staticmethod
-    def play(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/play/{}".format(uid, speaker.play)
-
-    @staticmethod
-    def pause(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/pause/{}".format(uid, speaker.pause)
-
-    @staticmethod
-    def track(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/track/{}".format(uid, speaker.track)
-
-    @staticmethod
-    def artist(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/artist/{}".format(uid, speaker.artist)
-
-    @staticmethod
-    def track_duration(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/track_duration/{}".format(uid, speaker.track_duration)
-
-    @staticmethod
-    def track_position(uid):
-        speaker = UdpResponse.get_speaker(uid)
-        if speaker:
-            return "speaker/{}/track_position/{}".format(uid, speaker.track_position)
-
 
 class UdpBroker():
 
