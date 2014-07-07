74e13db4124189e18eec206455af32da9d3129b6
pfischi <pfischi@gmx.de>
2014-07-07 23:41:15 +0200

    removed sonos_broker.sh from setup routine

--- server.sonos/setup.py
+++ server.sonos/setup.py
@@ -5,7 +5,7 @@ setup(
     name='sonos_broker',
     version='{version}'.format(version=definitions.VERSION),
     packages=['lib_sonos', 'soco'],
-    scripts=['sonos_broker', 'sonos_broker.sh', 'sonos_broker.cfg'],
+    scripts=['sonos_broker', 'sonos_broker.cfg'],
     url='https://github.com/pfischi/shSonos',
     license='',
     author='pfischi',
