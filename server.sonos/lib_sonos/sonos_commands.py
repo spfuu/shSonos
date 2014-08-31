d9915c88cdb9d35166cb69b068c0d558c103108a
pfischi <pfischi@gmx.de>
2014-08-31 13:03:39 +0200

    removed function

--- server.sonos/lib_sonos/sonos_commands.py
+++ server.sonos/lib_sonos/sonos_commands.py
@@ -4,12 +4,10 @@ import logging
 from urllib.parse import unquote_plus
 from lib_sonos import sonos_speaker
 from lib_sonos.sonos_library import SonosLibrary
-from lib_sonos.udp_broker import UdpBroker
 from lib_sonos.definitions import SCAN_TIMEOUT
 from lib_sonos import utils
 
 logger = logging.getLogger('')
-
 
 class Command():
     def __init__(self, service):
@@ -17,92 +15,6 @@ class Command():
         self.sonos_library = SonosLibrary()
         self.true_vars = ['true', '1', 't', 'y', 'yes']
 
-    def do_work(self, client_ip, path):
-
-        response = 'Unknown command'
-        try:
-            command = path
-            command = re.sub("^/|/$", '', command).split('/')
-
-            if command[0].lower() == 'speaker':
-                " input is : speaker/uid/property/action/value" \
-                " we're changing uid with property to get better handling with slices"
-                c1 = command[1]
-                c2 = command[2]
-                command[1] = c2
-                command[2] = c1
-
-            if len(command) > 1:
-                do_work = getattr(self, "{}_{}".format(command[0], command[1]))
-                return do_work(client_ip, command[2:])
-            else:
-                do_work = getattr(self, command[0])
-                return do_work(client_ip, None)
-
-        except Exception as err:
-            logger.error(err)
-            return False, response
-
-    def client_list(self, ip, arguments):
-        try:
-            logger.debug("arguments: {arguments} | ip: {ip}".format(arguments=', '.join(arguments), ip=ip))
-            data = ''
-            for uid, speaker in sonos_speaker.sonos_speakers.items():
-                data += "<p>uid             : {}</p>".format(speaker.uid)
-                data += "<p>ip              : {}</p>".format(speaker.ip)
-                data += "<p>model           : {}</p>".format(speaker.model)
-                data += "<p>current zone    : {}</p>".format(speaker.zone_name)
-                data += "<p>-------------------------------------------</p><p></p><p></p>"
-
-                logger.debug("active speakers: {uid} | {ip} | {model} | {zone}".
-                             format(uid=speaker.uid, ip=speaker.ip, model=speaker.model, zone=speaker.zone_name))
-            if not data:
-                logger.debug('no speakers online')
-                data = "No speakers online! Discover scan is performed every {} seconds.".format(SCAN_TIMEOUT)
-
-            return True, data
-        except Exception as err:
-            logger.error(err)
-            return False, "Couldn't list speakers"
-
-    def speaker_current_state(self, ip, arguments):
-        try:
-            logger.debug("arguments: {arguments} | ip: {ip}".format(arguments=', '.join(arguments), ip=ip))
-            uid = arguments[0].lower()
-
-            if not uid in sonos_speaker.sonos_speakers:
-                raise Exception("Couldn't find any speaker with uid '%s'!" % uid)
-
-            sonos_speaker.sonos_speakers[uid].send_data(force=True)
-
-            return True, "CURRENTSTATE command was processed successfully for speaker with uid '{}'.".format(uid)
-
-        except Exception as err:
-            logger.error(err)
-            return False, Exception(
-                "CURRENTSTATE command failed for speaker with uid '{}'!\nException: {}".format(uid, err))
-
-
-    def speaker_play_uri(self, ip, arguments):
-        try:
-            logger.debug("arguments: {arguments} | ip: {ip}".format(arguments=', '.join(arguments), ip=ip))
-            uid = arguments[0].lower()
-
-            if not uid in sonos_speaker.sonos_speakers:
-                raise Exception("Couldn't find any speaker with uid '%s'!" % uid)
-
-            if len(arguments) > 1:
-                uri = unquote_plus(arguments[1])
-                uri = utils.url_fix(uri)
-                sonos_speaker.sonos_speakers[uid].play_uri(uri.decode())
-            else:
-                raise Exception("Missing arguments")
-
-            # we need no explicit response here, play uri event triggers the update
-            return True, "PLAYURI command was processed successfully for speaker with uid '{}'.".format(uid)
-
-        except Exception as err:
-            return False, Exception("PLAYURI command failed for speaker with uid '{}'!\nException: {}".format(uid, err))
 
     def speaker_play_snippet(self, ip, arguments):
         try:
