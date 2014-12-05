2463a4612f27151b257b4b9f510c8df6ea89d288
pfischi <pfischi@gmx.de>
2014-12-05 20:54:11 +0100

    album cover should now shown correctly

--- widget.smartvisu/widget.min.js
+++ widget.smartvisu/widget.min.js
@@ -4,4 +4,4 @@ ADD this line to widget.js
 -----------------------------------------------
  */
 
-$(document).delegate('[data-widget="sonos.music"]',{update:function(e,r){if (r.toString()){document.getElementById(this.id).src=r.toString()+'?_='+new Date().getTime();}else{document.getElementById(this.id).src="pages/base/pics/trans.png";}}});
+$(document).delegate('[data-widget="sonos.cover"]',{update:function(d, a){$(this).attr('src', a);}});
