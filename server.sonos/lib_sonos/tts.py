1563bb1c1dce3c09f8fe057020912adc6ca5daf0
pfischi <pfischi@gmx.de>
2016-06-30 20:17:28 +0200

    GoogleTTS API changes

--- server.sonos/lib_sonos/tts.py
+++ server.sonos/lib_sonos/tts.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-import re
-import requests
 import calendar
 import math
 import time
+import requests
+import re
+
 
 class Token:
-
     """ Token (Google Translate Token)
     Generate the current token key and allows generation of tokens (tk) with it
     Python version of `token-script.js` itself from translate.google.com
@@ -16,16 +16,18 @@ class Token:
     SALT_2 = "+-3^+b+-f"
 
     def __init__(self):
-        # The current token key (hours since unix epoch)
-        timestamp = calendar.timegm(time.gmtime())
-        hours = int(math.floor(timestamp / 3600))
-        self.token_key = hours
+        self.token_key = None
 
     def calculate_token(self, text, seed=None):
         """ Calculate the request token (`tk`) of a string
         :param text: str The text to calculate a token for
-        :param seed: int The seed to use. By default this is the number of hours since epoch
+        :param seed: str The seed to use. By default this is the number of hours since epoch
         """
+
+        if seed is None:
+            seed = self._get_token_key()
+
+        [first_seed, second_seed] = seed.split(".")
 
         try:
             d = bytearray(text.encode('UTF-8'))
@@ -34,18 +36,35 @@ class Token:
             # to encode.
             d = bytearray(text)
 
-        a = seed if seed is not None else self.token_key
-        if seed is None:
-            seed = self.token_key
+        a = int(first_seed)
         for value in d:
             a += value
             a = self._work_token(a, self.SALT_1)
         a = self._work_token(a, self.SALT_2)
+        a ^= int(second_seed)
         if 0 > a:
             a = (a & 2147483647) + 2147483648
         a %= 1E6
         a = int(a)
-        return str(a) + "." + str(a ^ seed)
+        return str(a) + "." + str(a ^ int(first_seed))
+
+    def _get_token_key(self):
+        if self.token_key is not None:
+            return self.token_key
+
+        timestamp = calendar.timegm(time.gmtime())
+        hours = int(math.floor(timestamp / 3600))
+
+        response = requests.get("https://translate.google.com/")
+        line = response.text.split('\n')[-1]
+
+        tkk_expr = re.search(".*?(TKK=.*?;)W.*?", line).group(1)
+        a = re.search("a\\\\x3d(-?\d+);", tkk_expr).group(1)
+        b = re.search("b\\\\x3d(-?\d+);", tkk_expr).group(1)
+
+        result = str(hours) + "." + str(int(a) + int(b))
+        self.token_key = result
+        return result
 
     """ Functions used by the token calculation algorithm """
     def _rshift(self, val, n):
@@ -155,20 +174,19 @@ class gTTS:
     def write_to_fp(self, fp):
         """ Do the Web request and save to a file-like object """
         for idx, part in enumerate(self.text_parts):
-            payload = { 'ie': 'UTF-8',
-                        'q': part,
-                        'tl': self.lang,
-                        'total': len(self.text_parts),
-                        'idx': idx,
-                        'client': 't',
-                        'textlen': len(part),
-                        'tk': self.token.calculate_token(part)}
+            payload = { 'ie' : 'UTF-8',
+                        'q' : part,
+                        'tl' : self.lang,
+                        'total' : len(self.text_parts),
+                        'idx' : idx,
+                        'client' : 'tw-ob',
+                        'textlen' : len(part),
+                        'tk' : self.token.calculate_token(part)}
             headers = {
-                "Referer": "http://translate.google.com/",
+                "Referer" : "http://translate.google.com/",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
             }
-            if self.debug:
-                print(payload)
+            if self.debug: print(payload)
             try:
                 r = requests.get(self.GOOGLE_TTS_URL, params=payload, headers=headers)
                 if self.debug:
@@ -202,3 +220,6 @@ class gTTS:
             return [thestring[:idx]] + self._minimize(thestring[idx:], delim, max_size)
         else:
             return [thestring]
+
+if __name__ == "__main__":
+        pass
