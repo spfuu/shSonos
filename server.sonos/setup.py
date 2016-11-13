fa44b7881f41fd2d41de6a912f18edc3e454be29
pfischi <pfischi@gmx.de>
2016-11-13 21:56:42 +0100

    simpler name for installer by removing the date string

--- server.sonos/setup.py
+++ server.sonos/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from lib_sonos import definitions
 
 setup(
-    name='sonos_broker',
-    version='{version}'.format(version=definitions.VERSION_BUILDSTRING),
+    name='sonos-broker',
+    version='{version}'.format(version=definitions.VERSION),
     packages=['lib_sonos', 'soco', 'soco.music_services'],
     scripts=['sonos_broker', 'sonos_broker.cfg', 'sonos_cmd'],
     url='https://github.com/pfischi/shSonos',
